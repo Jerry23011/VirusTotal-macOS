4a24495eca9dcb4cfed687d141225f9f78851199
Jerry <jerry.23011@icloud.com>
2024-06-15 01:05:53 +0800

    feat: finish implementing mini mode

--- VirusTotal/Views/FileView/FileView.swift
+++ VirusTotal/Views/FileView/FileView.swift
@@ -191,36 +191,36 @@ struct FileView: View {
         }
         return true
     }
+}
 
-    private struct AnyDropDelegate: DropDelegate {
-        var isTargeted: Binding<Bool>?
-        var onValidate: ((DropInfo) -> Bool)?
-        let onPerform: (DropInfo) -> Bool
-        var onEntered: ((DropInfo) -> Void)?
-        var onExited: ((DropInfo) -> Void)?
-        var onUpdated: ((DropInfo) -> DropProposal?)?
+struct AnyDropDelegate: DropDelegate {
+    var isTargeted: Binding<Bool>?
+    var onValidate: ((DropInfo) -> Bool)?
+    let onPerform: (DropInfo) -> Bool
+    var onEntered: ((DropInfo) -> Void)?
+    var onExited: ((DropInfo) -> Void)?
+    var onUpdated: ((DropInfo) -> DropProposal?)?
 
-        func performDrop(info: DropInfo) -> Bool {
-            onPerform(info)
-        }
+    func performDrop(info: DropInfo) -> Bool {
+        onPerform(info)
+    }
 
-        func validateDrop(info: DropInfo) -> Bool {
-            onValidate?(info) ?? true
-        }
+    func validateDrop(info: DropInfo) -> Bool {
+        onValidate?(info) ?? true
+    }
 
-        func dropEntered(info: DropInfo) {
-            isTargeted?.wrappedValue = true
-            onEntered?(info)
-        }
+    func dropEntered(info: DropInfo) {
+        isTargeted?.wrappedValue = true
+        onEntered?(info)
+    }
 
-        func dropExited(info: DropInfo) {
-            isTargeted?.wrappedValue = false
-            onExited?(info)
-        }
+    func dropExited(info: DropInfo) {
+        isTargeted?.wrappedValue = false
+        onExited?(info)
+    }
 
-        func dropUpdated(info: DropInfo) -> DropProposal? {
-            onUpdated?(info)
-        }
+    func dropUpdated(info: DropInfo) -> DropProposal? {
+        onUpdated?(info)
     }
 }
 
--- VirusTotal/Views/MiniMode/MiniFileView.swift
+++ VirusTotal/Views/MiniMode/MiniFileView.swift
@@ -8,12 +8,160 @@
 import SwiftUI
 
 struct MiniFileView: View {
+    @ObservedObject private var viewModel = AnalyzeFileViewModel()
+    @Environment(\.openURL) private var openURL
+
+    @State private var isFileImporterPresent: Bool = false
+    @State private var isFileDropped: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            switch viewModel.statusMonitor {
+            case .none, .success:
+                noneAndSuccess
+            case .upload, .uploading, .fail, .analyzing:
+                upload
+            case .empty, .loading: // Will never be used in MiniFileView
+                EmptyView()
+            }
+        }
+        .frame(width: 230, height: 165)
+        .border(isFileDropped ? Color.accentColor : .clear, width: 5)
+
+    }
+
+    // MARK: ViewBuilders
+    @ViewBuilder
+    private var noneAndSuccess: some View {
+        VStack(alignment: .center) {
+            OpenFinderButton(title: "fileview.button.open.finder",
+                             systemImage: "folder",
+                             action: onPressFileImporter)
+            .padding(.top)
+            .scaleEffect(isFileDropped ? 1.05 : 1)
+            .animation(.spring, value: isFileDropped)
+            .keyboardShortcut("o")
+            .fileImporter(isPresented: $isFileImporterPresent,
+                          allowedContentTypes: [.data],
+                          allowsMultipleSelection: false) { result in
+                switch result {
+                case .success(let urls):
+                    urls.forEach { url in
+                        viewModel.handleFileImport(url)
+                    }
+                case .failure(let error):
+                    log.error("File Import Error: \(error.localizedDescription)")
+                }
+            }
+            Text("minimode.file.drag.file")
+            Spacer()
+        }
+        .padding(.horizontal)
+        .onDrop(of: [.fileURL], delegate: AnyDropDelegate(
+            isTargeted: $isFileDropped.animation(.linear(duration: 0.1)),
+            onValidate: { dropInfo in
+                validateDropInfo(dropInfo)
+            },
+            onPerform: { dropInfo in
+                handleDropInfo(dropInfo)
+            }
+        ))
+        .onChange(of: viewModel.statusMonitor) {
+            if shouldOpenURL() {
+                openURL(URL(string: makeVtURL(viewModel.inputSHA256)) ?? vtWebsite)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var upload: some View {
+        VStack(alignment: .center) {
+            Button(action: uploadFile) {
+                if viewModel.statusMonitor == .uploading {
+                    ProgressView()
+                        .controlSize(.small)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                } else if viewModel.statusMonitor == .fail {
+                    Image(systemName: "exclamationmark.triangle.fill")
+                        .font(.title)
+                        .foregroundStyle(.red)
+                        .symbolRenderingMode(.hierarchical)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                } else {
+                    Image(systemName: "doc.badge.arrow.up.fill")
+                        .font(.title2)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+            }
+            .buttonStyle(MiniModeButtonStyle())
+            .keyboardShortcut(.return, modifiers: .command)
+            .disabled(isUploading())
+            .padding(.top)
+            Text(viewModel.fileName ?? "n/a")
+                .lineLimit(1)
+            Spacer()
+        }
+        .padding(.horizontal)
+        .onChange(of: viewModel.statusMonitor) {
+            if shouldOpenURL() {
+                openURL(URL(string: makeVtURL(viewModel.inputSHA256)) ?? vtWebsite)
+            }
+        }
+    }
+
+    // MARK: Private
+
+    private let vtWebsite = URL(string: "https://virustotal.com")!
+
+    /// Return true when statusMonitor is .success or .analyzing, return false otherwise
+    private func shouldOpenURL() -> Bool {
+        let statusMonitor = viewModel.statusMonitor
+        return statusMonitor == .success || statusMonitor == .analyzing
+    }
+
+    /// Toggle isFileImporterPresent
+    private func onPressFileImporter() {
+        isFileImporterPresent.toggle()
+    }
+
+    /// Given a DropInfo, return true if DropInfo is `.data` and is a single item, return false otherwise
+    private func validateDropInfo(_ dropInfo: DropInfo) -> Bool {
+        let fileURLs = dropInfo.fileURLsConforming(to: [.data])
+        return fileURLs.count == 1
+    }
+
+    /// Given a DropInfo, handle the dropped item with onPerform
+    private func handleDropInfo(_ dropInfo: DropInfo) -> Bool {
+        guard let fileURL = dropInfo.fileURLsConforming(to: [.data]).first else {
+            return false
+        }
+        NSApp.activate(ignoringOtherApps: true)
+        viewModel.setupFileInfo(fileURL: fileURL) {
+            viewModel.getFileReport()
+        }
+        return true
+    }
+
+    /// Given a sha256 hash, return a URL that can be opened in external browser
+    /// to view analysis report on VirusTotal website
+    private func makeVtURL(_ sha256: String) -> String {
+        return "https://www.virustotal.com/gui/file/\(sha256)"
+    }
+
+    /// Trigger `uploadFile` and call `cancelOngoingRequest` after completion
+    private func uploadFile() {
+        viewModel.uploadFile {_ in
+            viewModel.cancelOngoingRequest()
+        }
+    }
+
+    /// Return true is statusMonitor is .uploading, false otherwise
+    private func isUploading() -> Bool {
+        return viewModel.statusMonitor == .uploading
     }
 }
 
 #Preview {
     MiniFileView()
-        .frame(width: 200, height: 160)
+        .frame(width: 230, height: 160)
 }
