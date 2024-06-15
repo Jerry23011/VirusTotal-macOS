6d7af2691f11991c26115b2187f652038b216342
Jerry <jerry.23011@icloud.com>
2024-06-15 20:40:15 +0800

    perf: set MiniFileView as the default mini view

--- VirusTotal/Views/MiniMode/MiniModeView.swift
+++ VirusTotal/Views/MiniMode/MiniModeView.swift
@@ -8,55 +8,39 @@
 import SwiftUI
 
 struct MiniModeView: View {
+    @State private var tabSelection: MiniTab = .file
+
     var body: some View {
-        if #available(macOS 15.0, *) {
-            legacyTab
-// Enable code below after Xcode 16's official release
-//            TabView {
-//                Tab("sidebar.home", systemImage: "house.fill") {
-//                    MiniHomeView()
-//                }
-//                Tab("sidebar.file", systemImage: "arrow.up.doc.fill") {
-//                    MiniFileView()
-//                }
-//                Tab("sidebar.url", systemImage: "link") {
-//                    MiniURLView()
-//                }
-//            }
-            .task {
-                if let window = NSApp.findWindow(WindowID.main) {
-                    window.titleVisibility = .hidden
-                    window.titlebarAppearsTransparent = true
+        TabView(selection: $tabSelection) {
+            MiniHomeView()
+                .tabItem {
+                    Label("sidebar.home", systemImage: "house.fill")
                 }
-            }
-        } else {
-            legacyTab // For macOS 14
-            .task {
-                if let window = NSApp.findWindow(WindowID.main) {
-                    window.titleVisibility = .hidden
-                    window.titlebarAppearsTransparent = true
+                .tag(MiniTab.home)
+            MiniFileView()
+                .tabItem {
+                    Label("sidebar.file", systemImage: "arrow.up.doc.fill")
                 }
+                .tag(MiniTab.file)
+            MiniURLView()
+                .tabItem {
+                    Label("sidebar.url", systemImage: "link")
+                }
+                .tag(MiniTab.url)
+        }
+        .task {
+            if let window = NSApp.findWindow(WindowID.main) {
+                window.titleVisibility = .hidden
+                window.titlebarAppearsTransparent = true
             }
         }
     }
 }
 
-@ViewBuilder
-private var legacyTab: some View {
-    TabView {
-        MiniHomeView()
-            .tabItem {
-                Label("sidebar.home", systemImage: "house.fill")
-            }
-        MiniFileView()
-            .tabItem {
-                Label("sidebar.file", systemImage: "arrow.up.doc.fill")
-            }
-        MiniURLView()
-            .tabItem {
-                Label("sidebar.url", systemImage: "link")
-            }
-    }
+enum MiniTab {
+    case home
+    case file
+    case url
 }
 
 #Preview {
