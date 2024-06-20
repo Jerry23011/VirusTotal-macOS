8997df4901362f78ab035723bd340ff1b2b53162
Jerry <jerry.23011@icloud.com>
2024-06-20 22:52:43 +0800

    format: checkForUpdates ViewModel

--- VirusTotal/Utilities/SparkleUpdates.swift
+++ VirusTotal/Utilities/SparkleUpdates.swift
@@ -14,22 +14,23 @@ final class CheckForUpdatesViewModel: ObservableObject {
     @Published var canCheckForUpdates = false
 
     init(updater: SPUUpdater) {
-        updater.publisher(for: \.canCheckForUpdates)
+        updater
+            .publisher(for: \.canCheckForUpdates)
             .assign(to: &$canCheckForUpdates)
     }
 }
 
 struct CheckForUpdatesView: View {
-    @ObservedObject private var checkForUpdatesViewModel: CheckForUpdatesViewModel
+    @ObservedObject private var viewModel: CheckForUpdatesViewModel
     private let updater: SPUUpdater
 
     init(updater: SPUUpdater) {
         self.updater = updater
-        self.checkForUpdatesViewModel = CheckForUpdatesViewModel(updater: updater)
+        self.viewModel = CheckForUpdatesViewModel(updater: updater)
     }
 
     var body: some View {
         Button("check.for.updates", action: updater.checkForUpdates)
-            .disabled(!checkForUpdatesViewModel.canCheckForUpdates)
+            .disabled(!viewModel.canCheckForUpdates)
     }
 }
